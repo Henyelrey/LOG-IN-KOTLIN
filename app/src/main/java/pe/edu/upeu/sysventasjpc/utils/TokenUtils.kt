fbd7bbeac22c00078864dce45b76e137b2e0fc12
Henyelrey <henyel91@gmail.com>
2025-04-21 13:04:55 -0500

    avance de la clase

--- app/src/main/java/pe/edu/upeu/sysventasjpc/utils/TokenUtils.kt
+++ app/src/main/java/pe/edu/upeu/sysventasjpc/utils/TokenUtils.kt
@@ -3,9 +3,9 @@ package pe.edu.upeu.sysventasjpc.utils
 import android.content.Context
 
 object TokenUtils {
-    var TOKEN_CONTENT="Aqui va el Token"
-    var API_URL="http://192.168.37.152:8080/"
+    var TOKEN_CONTENT = "Aqui va el Token"
+    var API_URL = "http://172.22.2.8:8080/"
     lateinit var CONTEXTO_APPX: Context
-    var USER_LOGIN=""
-    var ID_ASIS_ACT=0L
+    var USER_LOGIN = ""
+    var ID_ASIS_ACT = 0L
 }
